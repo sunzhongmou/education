8747816fa877eaf551f8fc82fcdea00d9724c802
sunwei <me@sunwei.xyz>
2020-11-20 07:45:28 +0800

    Modified: make it prettier; Wayde

--- packages/math/__tests__/permutation.test.ts
+++ packages/math/__tests__/permutation.test.ts
@@ -1,20 +1,22 @@
-import {getSequenceFromNumber, getFullPermutationFromSequence} from '../src/permutation'
+import {
+  getSequenceFromNumber,
+  getFullPermutationFromSequence
+} from '../src/permutation'
 
 describe('permutation tests', () => {
   it('get sequence from number', () => {
     const seq = getSequenceFromNumber(10)
 
-    expect(seq.indexOf(5) >= 0).toBeTruthy()
-    expect(seq.indexOf(0) >= 0).toBeTruthy()
-    expect(seq.indexOf(10) >= 0).toBeTruthy()
+    expect(seq.includes(5)).toBeTruthy()
+    expect(seq.includes(0)).toBeTruthy()
+    expect(seq.includes(10)).toBeTruthy()
   })
 
   it('get random permutation from number', () => {
-    let n = 10
+    const n = 10
     const seq = getSequenceFromNumber(n)
     const per = getFullPermutationFromSequence(seq)
 
     expect(per.length).toEqual((n + 1) * (n + 1))
   })
-
 })
--- packages/math/src/permutation.ts
+++ packages/math/src/permutation.ts
@@ -1,10 +1,11 @@
-export function getSequenceFromNumber(n: number): Array<number> {
+export function getSequenceFromNumber(n: number): number[] {
   return [...Array(n + 1).keys()]
 }
 
-export function getFullPermutationFromSequence(sequence: Array<number>): Array<Array<number>> {
+export function getFullPermutationFromSequence(sequence: number[]): number[][] {
   return sequence.reduce(
     (permutation, currentValue) =>
       permutation.concat(sequence.map(ele => [currentValue, ele])),
-    <number[][]>[]);
+    <number[][]>[]
+  )
 }
