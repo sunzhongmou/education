d56d055c7ae123920c2c3bc42cb49dbe1cbbfc62
sunwei <me@sunwei.xyz>
2020-11-20 08:45:29 +0800

    Modified: support get descending permutation and pick random element from sequence; Wayde

--- packages/math/__tests__/permutation.test.ts
+++ packages/math/__tests__/permutation.test.ts
@@ -1,22 +1,44 @@
 import {
-  getSequenceFromNumber,
-  getFullPermutationFromSequence
+  getSequence,
+  getFullPermutation,
+  getDescendingPermutation,
+  randomPickElement
 } from '../src/permutation'
 
 describe('permutation tests', () => {
   it('get sequence from number', () => {
-    const seq = getSequenceFromNumber(10)
+    const seq = getSequence(10)
 
     expect(seq.includes(5)).toBeTruthy()
     expect(seq.includes(0)).toBeTruthy()
     expect(seq.includes(10)).toBeTruthy()
+    expect(seq[5] > seq[4]).toBeTruthy()
   })
 
-  it('get random permutation from number', () => {
+  it('get full permutation from number', () => {
     const n = 10
-    const seq = getSequenceFromNumber(n)
-    const per = getFullPermutationFromSequence(seq)
+    const seq = getSequence(n)
+    const per = getFullPermutation(seq)
 
     expect(per.length).toEqual((n + 1) * (n + 1))
+  })
+
+  it('get descending permutation from number', () => {
+    const n = 10
+    const seq = getSequence(n).reverse()
+    const per = getDescendingPermutation(seq)
+    const ele = randomPickElement(per)
+
+    expect(per.length).toEqual((n * (n + 1)) / 2)
+    expect(ele[0] > ele[1]).toBeTruthy()
+  })
+
+  it('get random element from sequence', () => {
+    const n = 10
+    const seq = getSequence(n)
+    const per = getFullPermutation(seq)
+    const ele = randomPickElement(per)
+
+    expect(per.includes(ele)).toBeTruthy()
   })
 })
--- packages/math/src/permutation.ts
+++ packages/math/src/permutation.ts
@@ -1,11 +1,24 @@
-export function getSequenceFromNumber(n: number): number[] {
+export function getSequence(n: number): number[] {
   return [...Array(n + 1).keys()]
 }
 
-export function getFullPermutationFromSequence(sequence: number[]): number[][] {
+export function getFullPermutation(sequence: number[]): number[][] {
   return sequence.reduce(
     (permutation, currentValue) =>
       permutation.concat(sequence.map(ele => [currentValue, ele])),
     <number[][]>[]
   )
+}
+
+export function getDescendingPermutation(sequence: number[]): number[][] {
+  const permutation = <number[][]>[]
+  return permutation.concat(
+    ...sequence.map((currentValue, index) =>
+      sequence.slice(index + 1).map(followValue => [currentValue, followValue])
+    )
+  )
+}
+
+export function randomPickElement(sequence: number[][]): number[] {
+  return sequence[Math.floor(Math.random() * sequence.length)]
 }
